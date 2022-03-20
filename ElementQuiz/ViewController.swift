9da88ca0c172d60c1edcf0a98a0dcbcda474b473
j00hyun <loove1997@naver.com>
2022-03-20 22:30:21 +0900

    UI세팅

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -7,13 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Mode {
+    case flashCard
+    case quiz
+}
 
+enum State {
+    case question
+    case answer
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var modeSelector: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
+    
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    
+    var currentElementIndex = 0
+    var mode: Mode = .flashCard
+    
+    func updateFlashCardUI() {
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+        
+        answerLabel.text = "?"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateFlashCardUI()
     }
 
-
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    
+    @IBAction func next(_ sender: UIButton) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count {
+            currentElementIndex = 0
+        }
+        
+        updateFlashCardUI()
+    }
 }
 
